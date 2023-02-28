a548ad61d62d445574fd8100e4f5ddba4ec777a3
Jamie <>
2023-02-28 10:27:27 +0800

    62 init useEffect hook to fetchapi and watch state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
-// import { Component } from 'react';
+import { useState, useEffect } from 'react';
+
 // import CardList from './components/card-list/card-list.component'; 
-// import SearchBox from './components/search-box/search-box.component';
+import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
 const App = () => {
+
+  const [ searchField, setSearchField ] = useState(''); // [ value, setFunction ]
+  const [ monsters, setMonsters ] = useState([]);
+  const [ filteredMonsters, setFilterMonsters ] = useState(monsters)
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then((users) => setMonsters(users));
+  }, [] );
+
+  useEffect(()=>{
+    const newFilteredMonsters = monsters.filter((monster)=>{
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    })
+
+    setFilterMonsters(newFilteredMonsters)
+  },[monsters, searchField])
+  
+
+  const onSeachChange = (event)=>{
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString)
+  }
+
   return (
     <div className="App">
 
         <h1 className='app-title'>Monster Rolodex</h1>
-        {/* <SearchBox 
+
+        <SearchBox 
           className = 'monsters-search-box'
           onChangeHandler = { onSeachChange }
           placeholder = 'search monsters' 
         />
-        <CardList 
+         {/*<CardList 
           monsters={ filteredMonsters }
         /> */}
       </div>
